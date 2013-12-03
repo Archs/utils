32eefc91a89a96445443eeaf5e36319c4a8f79fa
Archs <sun@archs.tk>
2013-12-03 18:01:03 +0800

    add encoding support

--- spider/spider.go
+++ spider/spider.go
@@ -1,12 +1,16 @@
 package main
 
 import (
+	"code.google.com/p/mahonia"
 	"crypto/md5"
+	"errors"
 	"flag"
 	"fmt"
 	"github.com/opesun/goquery"
 	"io"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"os"
 	"strings"
 	"time"
@@ -26,6 +30,12 @@ var (
 	visited map[string]bool
 )
 
+// for decodings
+var (
+	gbk  = mahonia.NewDecoder("gb18030")
+	big5 = mahonia.NewDecoder("big5")
+)
+
 // store only md5 of the url to reduce memory usage
 func addNewUrl(url string) {
 	h := md5.New()
@@ -38,11 +48,72 @@ func addNewUrl(url string) {
 	}
 }
 
+// open
+func openUrl(url string) (data string, err error) {
+	var resp *http.Response
+	var raw []byte
+	var dec mahonia.Decoder = nil
+	resp, err = http.Get(url)
+	if err != nil {
+		return
+	}
+	if resp.StatusCode != http.StatusOK {
+		err = errors.New("Bad Status:" + resp.Status)
+		return
+	}
+	// only handle html files
+	ctype := resp.Header.Get("Content-Type")
+	if -1 == strings.Index(ctype, "text/html") {
+		err = errors.New("Nota a html file")
+		return
+	}
+	// try enconding: gbk\big5\utf8
+	charset := ""
+	if seps := strings.Split(ctype, "="); len(seps) >= 2 {
+		charset = seps[1]
+		charset = strings.ToLower(charset)
+		if strings.HasPrefix(charset, "gb") {
+			charset = "gb18030"
+			dec = gbk
+		} else if strings.HasPrefix(charset, "big") {
+			charset = "big5"
+			dec = big5
+		} else if strings.HasPrefix(charset, "utf") || charset == "" {
+			charset = "utf8"
+			dec = nil
+		} else {
+			err = errors.New("Unsupported charset:" + charset)
+			return
+		}
+	} else {
+		dec = nil
+	}
+	// TODO gzip handle
+	contentEncoding := resp.Header.Get("Content-Encoding")
+	if contentEncoding == "gzip" {
+		err = errors.New("Content-Encoding:" + contentEncoding + "temporally not supported")
+		return
+	}
+	// read the response
+	if dec != nil {
+		raw, err = ioutil.ReadAll(dec.NewReader(resp.Body))
+	} else {
+		raw, err = ioutil.ReadAll(resp.Body)
+	}
+	if err != nil {
+		return
+	}
+	defer resp.Body.Close()
+	data = string(raw)
+	return
+}
+
 func spider(word string) {
 	for {
 		url := <-urls
 		log.Println(url)
-		nodes, err := goquery.ParseUrl(url)
+		html, err := openUrl(url)
+		nodes, err := goquery.ParseString(html)
 		if err != nil {
 			log.Println("Err:", err.Error())
 			continue
@@ -66,7 +137,7 @@ func spider(word string) {
 			}()
 		})
 		// match word
-		if -1 != strings.Index(nodes.Html(), word) {
+		if -1 != strings.Index(html, word) {
 			log.Println("Found keyword in:", url)
 			writer.WriteString(url + "\n")
 		}
